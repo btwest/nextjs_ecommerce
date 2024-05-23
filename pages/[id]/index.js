10f51e5efcdae138cf4dd37b062ce4e76899a853
Brendan West <brendantwest@gmail.com>
2024-05-23 11:23:26 -0400

    Added Context Logic

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,6 +1,7 @@
 /*Displays detailed information about a specific product. 
 The specific product to display is determined by which PurchaseCard the user selects.  */
 
+import { useAppContext } from "@/context/CartContext";
 import React from "react";
 
 export async function getServerSideProps(context) {
@@ -11,5 +12,7 @@ export async function getServerSideProps(context) {
 }
 
 export default function Description(props) {
+  const { id: path } = props;
+  const { state: { prices } = [], dispatch } = useAppContext();
   return <div>HELLO!</div>;
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
+import { AppWrapper } from "../context/CartContext";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppWrapper>
   );
 }
